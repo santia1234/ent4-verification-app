13fd1f064066f281dd80a2d73094131803938896
santia1234 <santiagobox.5@gmail.com>
2024-05-21 22:41:34 -0500

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,30 @@
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  verifyCode,
+  login,
+  getLoggedUser,
+} = require("../controllers/user.controllers");
+const express = require("express");
+const verifyJWT = require("../utils/verifyjwt");
+
+const userRouter = express.Router();
+
+userRouter.route("/users").get(verifyJWT, getAll).post(create);
+
+userRouter.route("/users/login").post(login);
+
+userRouter.route("/user/me").get(verifyJWT, getLoggedUser);
+
+userRouter
+  .route("/users/:id")
+  .get(verifyJWT, getOne)
+  .delete(verifyJWT, remove)
+  .put(verifyJWT, update);
+
+userRouter.route("/users/verify/:code").get(verifyCode);
+
+module.exports = userRouter;
